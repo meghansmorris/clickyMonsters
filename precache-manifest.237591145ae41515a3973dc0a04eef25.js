8969d5b6c4a59acb24e85bad34ecb8a29d2b6fbc
itsmegh <42684917+itsmegh@users.noreply.github.com>
2019-02-22 14:13:49 -0700

    Updates

---
+++ precache-manifest.237591145ae41515a3973dc0a04eef25.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d96686be755b3bab3a86",
+    "url": "/clickyMonsters/static/js/runtime~main.d96686be.js"
+  },
+  {
+    "revision": "c0b8b2376a5469c83f1e",
+    "url": "/clickyMonsters/static/js/main.c0b8b237.chunk.js"
+  },
+  {
+    "revision": "9faaf100b571f1d79c3b",
+    "url": "/clickyMonsters/static/js/2.9faaf100.chunk.js"
+  },
+  {
+    "revision": "c0b8b2376a5469c83f1e",
+    "url": "/clickyMonsters/static/css/main.c816e933.chunk.css"
+  },
+  {
+    "revision": "66dec74eb24c684f650f554f49a6246f",
+    "url": "/clickyMonsters/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/clickyMonsters/precache-manifest.237591145ae41515a3973dc0a04eef25.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/clickyMonsters/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
